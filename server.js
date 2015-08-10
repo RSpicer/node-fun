c438804542b9f3e6faf1bd1ba5a27f5fc0696bbc
Ryan Spicer <rspicer@razegroup.com>
2015-08-10 10:53:32 -0600

    File structure

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = 8888;
+
+app.listen(port, function() {
+	console.log('It always feels like...' + port);
+});
