e432e9d64899601a7bceca07dc8df0e91cdf1b5f
Ryan Spicer <rspicer@razegroup.com>
2015-08-10 13:24:00 -0600

    Starting with twilio

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+var request = require('request');
 
 var app = express();
 var port = 8888;
+
+////////////////////////////
+//		MIDDLEWARE
+////////////////////////////
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}))
+app.use(bodyParser.json());
+app.use(cors());
+
+////////////////////////////
+//		ENDPOINTS
+////////////////////////////
+
+
+app.get('/api/message', function (req, res) {
+	return res.send({"message": "HELLOOOOOOOOOOOOOOOOOOOOOOOOOO!"})
+});
+
+app.post('/api/send_text_message', function (req, res) {
+	console.log(req.body.message);
+	res.send();
+})
 
 app.listen(port, function() {
 	console.log('It always feels like...' + port);
